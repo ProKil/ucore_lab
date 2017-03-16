35be0792df9bf6e2b2ff50856fc34e2baad6b473
tonygao <gaotong_sh@foxmail.com>
2017-03-16 13:57:20 +0800

    Copy from lab1 to lab2

--- labcodes/lab2/kern/debug/kdebug.c
+++ labcodes/lab2/kern/debug/kdebug.c
@@ -293,7 +293,7 @@ read_eip(void) {
  * */
 void
 print_stackframe(void) {
-     /* LAB1 YOUR CODE : STEP 1 */
+     /* LAB1 2014011357 : STEP 1 */
      /* (1) call read_ebp() to get the value of ebp. the type is (uint32_t);
       * (2) call read_eip() to get the value of eip. the type is (uint32_t);
       * (3) from 0 .. STACKFRAME_DEPTH
@@ -305,5 +305,18 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+	uint32_t ebp = read_ebp();
+	uint32_t eip = read_eip();
+	for (size_t i = 0; i < STACKFRAME_DEPTH; ++i) {
+		cprintf("ebp:0x%08x eip:0x%08x args:", ebp, eip);
+		uint32_t *args = (uint32_t *)ebp + 2;
+		for (size_t j = 0; j < 4; ++j) {
+			cprintf("0x%08x ", args[j]);
+		}
+		cprintf("\n");
+		print_debuginfo(eip - 1);
+		eip = *((uint32_t *)ebp + 1);
+		ebp = *((uint32_t *)ebp);
+	}
 }
 
