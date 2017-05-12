f08d090754f6ec999e3f5b78be8f1faa1b62b1d5
tonygao <gaotong_sh@foxmail.com>
2017-05-12 16:07:44 +0800

    Finish disksim homework

--- related_info/lab8/disksim-homework.py
+++ related_info/lab8/disksim-homework.py
@@ -18,7 +18,7 @@ STATE_DONE   = 4
 #
 # TODO
 # XXX transfer time
-# XXX satf 
+# XXX satf
 # XXX skew
 # XXX scheduling window
 # XXX sstf
@@ -51,7 +51,7 @@ class Disk:
 
         # figure out zones first, to figure out the max possible request
         self.InitBlockLayout()
-        
+
         # figure out requests
         random.seed(options.seed)
         self.requests     = self.MakeRequests(self.addr, self.addrDesc)
@@ -163,7 +163,7 @@ class Disk:
         # IO QUEUE
         self.queueX       = 20
         self.queueY       = 450
-        
+
         self.requestCount = 0
         self.requestQueue = []
         self.requestState = []
@@ -250,7 +250,7 @@ class Disk:
         print '100 as the maximum value, and 0 as the minumum. A max of -1 means just use the highest'
         print 'possible value as the max address to generate.'
         sys.exit(1)
-    
+
     #
     # ZONES AND BLOCK LAYOUT
     #
@@ -280,7 +280,7 @@ class Disk:
         skew                       = self.skew
         angleOffset                = 2 * self.blockAngleOffset[track]
         for angle in range(0, 360, angleOffset):
-            block = (angle / angleOffset) + pblock 
+            block = (angle / angleOffset) + pblock
             self.blockToTrackMap[block] = track
             self.blockToAngleMap[block] = angle + (angleOffset * skew)
             self.blockInfoList.append((track, angle + (angleOffset * skew), block))
@@ -299,7 +299,7 @@ class Disk:
         self.maxBlock              = pblock
         # print 'MAX BLOCK:', self.maxBlock
 
-        # adjust angle to starting position relative 
+        # adjust angle to starting position relative
         for i in self.blockToAngleMap:
             self.blockToAngleMap[i] = (self.blockToAngleMap[i] + 180) % 360
 
@@ -317,7 +317,7 @@ class Disk:
             (numRequests, maxRequest, minRequest) = (int(desc[0]), int(desc[1]), int(desc[2]))
             if maxRequest == -1:
                 maxRequest = self.maxBlock
-            # now make list 
+            # now make list
             tmpList = []
             for i in range(numRequests):
                 tmpList.append(int(random.random() * maxRequest) + minRequest)
@@ -379,7 +379,7 @@ class Disk:
             (cx, cy)   = (self.queueX + (col * self.queueBoxSize), self.queueY + (row * self.queueBoxSize))
             self.queueBoxID[index] = self.canvas.create_rectangle(cx - sizeHalf, cy - sizeHalf, cx + sizeHalf, cy + sizeHalf, fill='white')
             self.queueTxtID[index] = self.canvas.create_text(cx, cy, anchor='center', text=str(block))
-    
+
     def SwitchColors(self, c):
         if self.graphics:
             self.canvas.itemconfig(self.queueBoxID[self.currentIndex], fill=c)
@@ -421,7 +421,7 @@ class Disk:
             # print ' --> DONE WITH ROTATION!', self.timer
             return True
         return False
-        
+
     def PlanSeek(self, track):
         self.seekBegin = self.timer
         self.SwitchColors('orange')
@@ -453,6 +453,62 @@ class Disk:
             self.armTrack = self.armTarget
             return True
         return False
+
+    def DoCSCAN(self, rList):
+        minBlock = -1
+        minIndex = -1
+        minEst   = -1
+        minDeltaTrack = -1
+
+        # print '**** DoSATF ****', rList
+        for (block, index) in rList:
+            if self.requestState[index] == STATE_DONE:
+                continue
+            track = self.blockToTrackMap[block]
+            angle = self.blockToAngleMap[block]
+            # print 'track', track, 'angle', angle
+
+            deltaTrack = track - self.armTrack
+            if deltaTrack < 0:
+                deltaTrack += 3
+
+            if not (minDeltaTrack == -1 or deltaTrack <= minDeltaTrack):
+                continue
+
+            # estimate seek time
+            dist     = int(math.fabs(self.armTrack - track))
+            seekEst  = Decimal(self.trackWidth / self.armSpeedBase) * dist
+
+            # estimate rotate time
+            angleOffset    = self.blockAngleOffset[track]
+            angleAtArrival = (Decimal(self.angle) + (seekEst * self.rotateSpeed))
+            while angleAtArrival > 360.0:
+                angleAtArrival -= 360.0
+            rotDist        = Decimal((angle - angleOffset) - angleAtArrival)
+            while rotDist > 360.0:
+                rotDist -= Decimal(360.0)
+            while rotDist < 0.0:
+                rotDist += Decimal(360.0)
+            rotEst         = rotDist / self.rotateSpeed
+
+            # finally, transfer
+            xferEst = (Decimal(angleOffset) * Decimal(2.0)) / self.rotateSpeed
+
+            totalEst = seekEst + rotEst + xferEst
+
+            # should probably pick one on same track in case of a TIE
+            if minEst == -1 or totalEst < minEst:
+                minEst        = totalEst
+                minBlock      = block
+                minIndex      = index
+                minDeltaTrack = deltaTrack
+            # END loop
+
+        # when done
+        self.totalEst = minEst
+        assert(minBlock != -1)
+        assert(minIndex != -1)
+        return (minBlock, minIndex)
 
     def DoSATF(self, rList):
         minBlock = -1
@@ -501,11 +557,11 @@ class Disk:
         assert(minIndex != -1)
         return (minBlock, minIndex)
 
-    # 
+    #
     # actually doesn't quite do SSTF
     # just finds all the blocks on the nearest track
     # (whatever that may be) and returns it as a list
-    # 
+    #
     def DoSSTF(self, rList):
         minDist   = MAXTRACKS
         minBlock  = -1
@@ -530,7 +586,7 @@ class Disk:
             self.currWindow += 1
             if self.graphics:
                 self.DrawWindow()
-        
+
     def GetWindow(self):
         if self.currWindow <= -1:
             return len(self.requestQueue)
@@ -555,7 +611,7 @@ class Disk:
             self.isDone = True
             return
 
-        # do policy: should set currentBlock, 
+        # do policy: should set currentBlock,
         if self.policy == 'FIFO':
             (self.currentBlock, self.currentIndex) = self.requestQueue[self.requestCount]
             self.DoSATF(self.requestQueue[self.requestCount:self.requestCount+1])
@@ -566,6 +622,9 @@ class Disk:
             trackList = self.DoSSTF(self.requestQueue[0:self.GetWindow()])
             # then, do SATF on those blocks (otherwise, will not do them in obvious order)
             (self.currentBlock, self.currentIndex) = self.DoSATF(trackList)
+        # My Code
+        elif self.policy == 'CSCAN':
+            (self.currentBlock, self.currentIndex) = self.DoCSCAN(self.requestQueue[0:self.GetWindow()])
         else:
             print 'policy (%s) not implemented' % self.policy
             sys.exit(1)
@@ -575,7 +634,7 @@ class Disk:
 
         # add another block?
         if len(self.lateRequests) > 0 and self.lateCount < len(self.lateRequests):
-            self.AddRequest(self.lateRequests[self.lateCount])
+            self.AddRequest(int(self.lateRequests[self.lateCount]))
             self.lateCount += 1
 
     def Animate(self):
@@ -633,9 +692,9 @@ class Disk:
                         (self.rotBegin, self.seekBegin, self.xferBegin) = (self.timer, self.timer, self.timer)
                         self.SwitchState(STATE_XFER)
                         self.SwitchColors('green')
-                        
-                        
-        
+
+
+
         # make sure to keep the animation going!
         if self.graphics:
             self.root.after(20, self.Animate)
@@ -658,16 +717,16 @@ class Disk:
         self.rotTotal  += rotTime
         self.xferTotal += xferTime
 
-        
+
 
     def PrintStats(self):
         if self.compute == True:
             print '\nTOTALS      Seek:%3d  Rotate:%3d  Transfer:%3d  Total:%4d\n' % (self.seekTotal, self.rotTotal, self.xferTotal, self.timer)
-        
+
 # END: class Disk
 
 
-    
+
 #
 # MAIN SIMULATOR
 #
